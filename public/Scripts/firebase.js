b03231916a7ba261dc3c21865c97aed3f29b04a8
mishrashubham17 <shubhammishra69897@gmail.com>
2021-08-27 21:27:06 +0530

    updated blog app

---
+++ public/Scripts/firebase.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCN6KbibbAXiQuhFh2MrKndMnQZor-l7NU",
+    authDomain: "fir-blog-app-93a6d.firebaseapp.com",
+    databaseURL: "https://fir-blog-app-93a6d-default-rtdb.firebaseio.com",
+    projectId: "fir-blog-app-93a6d",
+    storageBucket: "fir-blog-app-93a6d.appspot.com",
+    messagingSenderId: "246033199550",
+    appId: "1:246033199550:web:f1e837020a6614bbd909ef"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
